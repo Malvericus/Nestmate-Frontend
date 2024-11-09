85eecbebbfcf57cf2dc3d298061db72c7a6370bc
kev0-4 <kevintandon123@gmail.com>
2024-11-09 21:39:09 +0530

    fix errors

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -26,7 +26,7 @@ function SignIn() {
     setError('');
 
     try {
-      const response = await fetch("http://127.0.0.1:8787/auth/signin", {
+      const response = await fetch("https://nestmatebackend.ktandon2004.workers.dev/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
